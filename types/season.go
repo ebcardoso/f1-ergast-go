a5afe9f6d0915f4bcc934d0cec43d200ece549a4
Breno Cardoso <elisiobreno@gmail.com>
2023-08-14 18:09:19 -0300

    Struct Seasons

---
+++ types/season.go
@@ -0,0 +1,22 @@
+package types
+
+type Season struct {
+	Season string `json:"season,omitempty"`
+	URL    string `json:"url,omitempty"`
+}
+
+type SeasonsData struct {
+	Limit       string `json:"limit,omitempty"`
+	Offset      string `json:"offset,omitempty"`
+	Total       string `json:"total,omitempty"`
+	Series      string `json:"series,omitempty"`
+	Url         string `json:"url,omitempty"`
+	Xmlns       string `json:"xmlns,omitempty"`
+	SeasonTable struct {
+		Seasons []Season `json:"Seasons,omitempty"`
+	} `json:"SeasonTable,omitempty"`
+}
+
+type SeasonsResponse struct {
+	Data SeasonsData `json:"MRData,omitempty"`
+}
