81630407c6d6c282dc3a782cd24491fcd2aa01e0
Breno Cardoso <elisiobreno@gmail.com>
2024-10-02 14:48:49 -0300

    Refactoring Drivers type response

--- types/driver.go
+++ types/driver.go
@@ -3,19 +3,10 @@ package types
 type DriversData struct {
 	paginationFields
 	DriverTable struct {
-		Season   string `json:"season,omitempty"`
-		Round    string `json:"round,omitempty"`
-		DriverId string `json:"driverId,omitempty"`
-		Drivers  []struct {
-			DriverId        string `json:"driverId,omitempty"`
-			PermanentNumber string `json:"permanentNumber,omitempty"`
-			Code            string `json:"code,omitempty"`
-			URL             string `json:"url,omitempty"`
-			GivenName       string `json:"givenName,omitempty"`
-			FamilyName      string `json:"FamilyName,omitempty"`
-			DateOfBirth     string `json:"dateOfBirth,omitempty"`
-			Nationality     string `json:"nationality,omitempty"`
-		} `json:"Drivers,omitempty"`
+		Season   string   `json:"season,omitempty"`
+		Round    string   `json:"round,omitempty"`
+		DriverId string   `json:"driverId,omitempty"`
+		Drivers  []Driver `json:"Drivers,omitempty"`
 	} `json:"DriverTable,omitempty"`
 }
 
